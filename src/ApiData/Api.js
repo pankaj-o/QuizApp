2ee0b9a9452f469a3f697814dfa6af5950da8a25
pankaj <er.pankaj2021@gmail.com>
2022-07-07 21:26:35 +0530

    little changes

--- src/ApiData/Api.js
+++ src/ApiData/Api.js
@@ -1,8 +1,8 @@
-import React from 'react'
+
 import axios from "axios"
 
 export default function Api() {
-    const response=axios.get("https://quizapi.io/api/v1/questions?apiKey=pp1d4IvmpXgtelfNIBolTGEUWo0j8g3VpzKYo80C&category=linux&difficulty=Easy&limit=20&tags=Linux").then((resp)=>{}).catch((Error)=>{console.log(Error)})
+    const response=axios.get("https://quizapi.io/api/v1/questions?apiKey=pp1d4IvmpXgtelfNIBolTGEUWo0j8g3VpzKYo80C&category=linux&difficulty=Easy&limit=20&tags=Linux").then((resp)=>{console.log(resp)}).catch((Error)=>{console.log(Error)})
 
 
   return response
--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./Card.css";
 import axios from "axios";
-
+// import Api from "../ApiData/Api";
 
 export default function Card() {
- let  v=0
-  const [Adata, setAdata] = useState([]);
+  
+  const [Adata, setAdata] = useState('');
+  let [other, setOther] = useState(0);
+  const [ans,setAns] =useState('')
+
+  
 
   let currentDate = new Date();
   let time =
@@ -17,29 +21,26 @@ export default function Card() {
 
   useEffect(() => {
     const dataf = async () => {
-      const response = await axios.get(
-        "https://quizapi.io/api/v1/questions?apiKey=pp1d4IvmpXgtelfNIBolTGEUWo0j8g3VpzKYo80C&category=linux&difficulty=Easy&limit=20&tags=Linux"
-      );
-
+        const response= await axios.get("https://quizapi.io/api/v1/questions?apiKey=pp1d4IvmpXgtelfNIBolTGEUWo0j8g3VpzKYo80C&category=linux&difficulty=Easy&limit=20&tags=Linux")
       setAdata(response.data);
+      
     };
     dataf();
-  }, []);
    
-   function Next(){
-    
-    setAdata(Adata[v++])
-   }
-   function Prev(){
-    
-    setAdata(Adata[v--])
-   }
+  }, [currentDate.getSeconds()]);
 
+  const Next=()=> {
+    setOther(other++);
+  }
+  const Prev=()=> {
+    setOther(other--);
+  }
+ 
   return (
     <>
       <div className="Outer_body">
         <div className="heading">
-          <h1>Welcome to Quiz App</h1>
+          <h1>Welcome to Quiz App {console.log(Adata?.data?.length())}</h1>
         </div>
 
         <div className="card_container">
@@ -48,34 +49,39 @@ export default function Card() {
               <h2> Question 1/3</h2>
             </div>
             <div className="Question">
-              <h3>{Adata.data[0].question}</h3>
+              <h3>{Adata[other]?.question}</h3>
             </div>
           </div>
           <div className="card_container_right">
             <div className="timedive">
               <p>
-               
                 Date= {currentDate.toLocaleDateString()} Time={time}
               </p>
             </div>
             <ul className="card_container_right_items">
-              <button className="bttn">{Adata.data[v].answers.answer_a} </button>
-              <button className="bttn">
-                
-                {Adata.data[v].answers.answer_b}
-              </button>
-              <button className="bttn"> {Adata.data[v].answers.answer_c}</button>
-              <button className="bttn">
-                
-                {Adata.data[v].answers.answer_d}
-              </button>
+              <button onClick={()=>{setAns("A")}} className="bttn">{Adata[other]?.answers?.answer_a}</button>
+              <button  onClick={()=>{setAns("B")}} className="bttn">{Adata[other]?.answers?.answer_b}</button>
+              <button onClick={()=>{setAns("C")}} className="bttn">{Adata[other]?.answers?.answer_c}</button>
+              <button onClick={()=>{setAns("D")}}  className="bttn">{Adata[other]?.answers?.answer_d}</button>
             </ul>
             <div className="submitbtn">
               <button className="bttn btnnps">Submit</button>
             </div>
             <div className="nextprevbtn">
-              <button disabled={Adata.length-1}className="bttn btnnps" onClick={Next}>Prev</button>
-              <button disabled={v===0}className="bttn btnnps" onClick={Prev}>Next</button>
+              <button
+               
+                disabled={other === 0}
+                className="bttn btnnps"
+                onClick={Prev}
+              >
+                Prev 
+              </button>
+              <button 
+          disabled={other === 20}
+        
+               className="bttn btnnps" onClick={Next}>
+                Next
+              </button>
             </div>
           </div>
         </div>
